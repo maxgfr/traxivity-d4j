d1e03499a9324e1d626bff29df934d3a4f97a268
maxgfr <maxgolfier@gmail.com>
2017-05-29 12:33:59 +0100

    :shark:

--- wear/src/main/java/com/maxgfr/traxivityd4j/MainActivity.java
+++ wear/src/main/java/com/maxgfr/traxivityd4j/MainActivity.java
@@ -1,10 +1,21 @@
 package com.maxgfr.traxivityd4j;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
 import android.support.wearable.view.BoxInsetLayout;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.layers.DenseLayer;
+import org.deeplearning4j.nn.conf.layers.OutputLayer;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.nd4j.linalg.activations.Activation;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.dataset.DataSet;
+import org.nd4j.linalg.factory.Nd4j;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -28,6 +39,12 @@ public class MainActivity extends WearableActivity {
         mContainerView = (BoxInsetLayout) findViewById(R.id.container);
         mTextView = (TextView) findViewById(R.id.text);
         mClockView = (TextView) findViewById(R.id.clock);
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                createAndUseNetwork();
+            }
+        });
     }
 
     @Override
@@ -60,5 +77,80 @@ public class MainActivity extends WearableActivity {
             mTextView.setTextColor(getResources().getColor(android.R.color.black));
             mClockView.setVisibility(View.GONE);
         }
+    }
+    private void createAndUseNetwork() {
+        DenseLayer inputLayer = new DenseLayer.Builder()
+                .nIn(2)
+                .nOut(3)
+                .name("Input")
+                .build();
+
+        DenseLayer hiddenLayer = new DenseLayer.Builder()
+                .nIn(3)
+                .nOut(2)
+                .name("Hidden")
+                .build();
+
+        OutputLayer outputLayer = new OutputLayer.Builder()
+                .nIn(2)
+                .nOut(2)
+                .name("Output")
+                .activation(Activation.SOFTMAX)
+                .build();
+
+        NeuralNetConfiguration.Builder nncBuilder = new NeuralNetConfiguration.Builder();
+        nncBuilder.iterations(60000);
+        nncBuilder.learningRate(0.1);
+
+        NeuralNetConfiguration.ListBuilder listBuilder = nncBuilder.list();
+        listBuilder.layer(0, inputLayer);
+        listBuilder.layer(1, hiddenLayer);
+        listBuilder.layer(2, outputLayer);
+
+        listBuilder.backprop(true);
+
+        MultiLayerNetwork myNetwork = new MultiLayerNetwork(listBuilder.build());
+        myNetwork.init();
+
+        final int NUM_SAMPLES = 4;
+
+        INDArray trainingInputs = Nd4j.zeros(NUM_SAMPLES, inputLayer.getNIn());
+        INDArray trainingOutputs = Nd4j.zeros(NUM_SAMPLES, outputLayer.getNOut());
+
+        // If 0,0 show 0
+        trainingInputs.putScalar(new int[]{0,0}, 0);
+        trainingInputs.putScalar(new int[]{0,1}, 0);
+        trainingOutputs.putScalar(new int[]{0,0}, 0);
+        trainingOutputs.putScalar(new int[]{0,1}, 1);
+
+        // If 0,1 show 1
+        trainingInputs.putScalar(new int[]{1,0}, 0);
+        trainingInputs.putScalar(new int[]{1,1}, 1);
+        trainingOutputs.putScalar(new int[]{1,0}, 1);
+        trainingOutputs.putScalar(new int[]{1,1}, 0);
+
+        // If 1,0 show 1
+        trainingInputs.putScalar(new int[]{2,0}, 1);
+        trainingInputs.putScalar(new int[]{2,1}, 0);
+        trainingOutputs.putScalar(new int[]{2,0}, 1);
+        trainingOutputs.putScalar(new int[]{2,1}, 0);
+
+        // If 1,1 show 0
+        trainingInputs.putScalar(new int[]{3,0}, 1);
+        trainingInputs.putScalar(new int[]{3,1}, 1);
+        trainingOutputs.putScalar(new int[]{3,0}, 0);
+        trainingOutputs.putScalar(new int[]{3,1}, 1);
+
+        DataSet myData = new DataSet(trainingInputs, trainingOutputs);
+        myNetwork.fit(myData);
+
+        // Create input
+        INDArray actualInput = Nd4j.zeros(1,2);
+        actualInput.putScalar(new int[]{0,0}, 1);
+        actualInput.putScalar(new int[]{0,1}, 1);
+
+        // Generate output
+        INDArray actualOutput = myNetwork.output(actualInput);
+        Log.d("myNetwork Output", actualOutput.toString());
     }
 }
